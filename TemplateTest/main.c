605d61de66672e2def5a8f36e917e28f61a8efbf
namkyungjin <nkj06@naver.com>
2020-01-31 11:11:15 +0900

    2장 작업

--- TemplateTest/main.c
+++ TemplateTest/main.c
@@ -7,8 +7,13 @@
 
 int main() {
 	printf("Hello World!\n");
+	printf("Hi second again\n");
 
-	// type here
+	printf("Be happy\n");
+	printf("12345678901234567890\n");
+	printf("My\tfriend\n");
+	printf("Goot\bd\tchance\n");
+	printf("Cow\rW\a\n");
 
 	system("pause");
 	return 0;
