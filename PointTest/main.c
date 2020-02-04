cb79fcfef3941c6827757cd941f7b1d510c29b6d
namkyungjin <nkj06@naver.com>
2020-02-04 10:51:17 +0900

    9장 추가

--- PointTest/main.c
+++ PointTest/main.c
@@ -13,13 +13,21 @@
 // 메인함수
 int main(void) 
 {
-    int a;
-    double b;
-    char c;
+    int a = 10, b = 15, total;
+    double avg;
+    int* pa, * pb; 
+    int* pt = &total;
+    double* pg = &avg;
 
-    printf("int형 변수의 주소 : %u\n", &a);
-    printf("double형 변수의 주소 : %u\n", &b);
-    printf("char형 변수의 주소 : %u\n", &c);
+    pa = &a; // 포인터 pa에 변수 a의 주소 대입
+    pb = &b; // 포인터 pb에 변수 b의 주소 대입
+
+    *pt = *pa + *pb; // a값과 b값을 더해 total에 저장
+    *pg = *pt / 2.0; // total 값을 2로 나눈 값을 avg에 저장
+
+    printf("두 정수의 값 : %d, %d\n", *pa, *pb); // a값과 b값 출력
+    printf("두 정수의 합 : %d \n", *pt); // total 값 출력 
+    printf("두 정수의 평균 : %.1lf\n", *pg); // avg 값 출력 
 
 	system("pause");
 	return EXIT_SUCCESS;
