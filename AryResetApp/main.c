9ba4e7aa2bae9bbf0a26e4e8da6adc7c335cf87e
namkyungjin <nkj06@naver.com>
2020-02-06 09:14:38 +0900

    추가

--- AryResetApp/main.c
+++ AryResetApp/main.c
@@ -29,6 +29,22 @@ int main(void)
         }
         printf("\n");
     }
+
+    char animal[5][20];
+    int count;
+
+    count = sizeof(animal) / sizeof(animal[0]); // 행의 수 계산
+
+    for (i = 0; i < count; i++) 
+    {
+        scanf("%s", animal[i]);
+    }
+
+    for (i = 0; i < count; i++)
+    {
+        printf("%s  ", animal[i]);
+    }
+
 	system("pause");
 	return EXIT_SUCCESS;
 }
